09009fdee1773f7119a1d15f823494562c1a1d7f
xudacheng <xudacheng_tomous@163.com>
2019-07-25 11:30:02 +0800

    Initial Commit

---
+++ PayMentTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PayMentTool
+//
+//  Created by 大橙子 on 2019/7/25.
+//  Copyright © 2019 Tomous. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PayMentTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayMentTool
+//
+//  Created by 大橙子 on 2019/7/25.
+//  Copyright © 2019 Tomous. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
